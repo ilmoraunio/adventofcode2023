cae3087a1fdd0c3fa3457fc330ec0d36dba427aa
Ilmo Raunio <ilmo.raunio@metosin.fi>
2023-12-06 15:30:02 +0200

    Day 6 solution

---
+++ src/aoc2023/day_6.clj
@@ -0,0 +1,30 @@
+(ns aoc2023.day-6
+  (:require [clojure.string :as str]))
+
+(defn times-and-distances
+  [s]
+  (->> (->> s
+            (re-seq #"Time:(.*)\nDistance:(.*)")
+            first
+            ((juxt second #(nth % 2)))
+            (map str/trim))
+       (map #(str/split % #"\h+"))
+       (map (partial map #(Integer. %)))
+       (apply zipmap)
+       (map #(-> {:time (first %) :distance (second %)}))))
+
+(defn determine-winning-scenarios
+  [filename]
+  (->> (times-and-distances (slurp filename))
+       (map (fn [{:keys [time distance]}]
+              (keep (fn [time-held-ms]
+                      (let [speed (* time-held-ms 1)
+                            time-left-ms (- time time-held-ms)
+                            distance-travelled-mm (* speed time-left-ms)]
+                        (when (> distance-travelled-mm distance)
+                          {:time-held-ms time-held-ms
+                           :speed speed
+                           :distance distance-travelled-mm})))
+                    (range time))))
+       (map count)
+       (apply *)))
